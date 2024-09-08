ce0c9f44aac3d8cdf976d2f87e1e432b85ff64d9
zaher555 <zahermohmed555@gmail.com>
2024-09-09 00:45:52 +0300

    landing page design

---
+++ resources/views/components/landing.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+CU:wght@100..400&display=swap" rel="stylesheet">
+    <style>
+        html,body
+        {
+            height: 100%;
+        }
+        nav
+        {
+            background-color: #12072A !important;
+        }
+        nav a:hover
+        {
+            color: rgb(255 193 7) !important;
+        }
+        .landing
+        {
+            background-repeat: no-repeat;
+            height: 100vh;
+            position: relative;
+        }
+        .landing::after
+        {
+            content: "";
+            width: 100%;
+            height: 100%;
+            background-color: gray;
+            top: 0;
+            left: 0;
+            z-index: 2;
+            position: absolute
+        }
+        .landing .content
+        {
+            z-index: 3;
+            position: absolute;
+        }
+        .landing button
+        {
+            width: 100px;
+            font-weight: bolder
+        }
+        .landing button:last-of-type:hover
+        {
+            background-color: transparent !important;
+            border-color: rgb(255 193 7);
+            color: white !important;
+
+        }
+        .landing button:first-of-type
+        {
+            background-color: #12072A;
+        }
+        .landing button:first-of-type:hover
+        {
+            background-color: transparent;
+            border-color: #12072A;
+        }
+        .background-video {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover; /* Ensures the video covers the div */
+            z-index: 3; /* Sends the video behind the content */
+        }
+    </style>
+</head>
+<body>
+    <x-navbar>$slot</x-navbar>
+    <div class="landing text-center d-flex justify-content-center align-items-center">
+        <video class="background-video" autoplay loop muted>
+            <source src="https://cdn.pixabay.com/video/2016/08/22/4723-179738625_tiny.mp4" type="video/mp4">
+        </video>
+        <div class="content m-auto">
+            <h2 class="text-white">I Want To:</h2>
+            <button class="btn text-white rounded pt-1 pb-1">Hire</button>
+            <button class="btn text-dark rounded pt-1 pb-1 bg-warning">Work</button>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <span style=" font-family: Playwrite CU, cursive; font-optical-sizing: auto;
+        font-style: normal;text-decoration:none" class="text-white">Job 4 U</span>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="#"><i class="fa-solid fa-user-plus"></i> sign up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#"><i class="fa-solid fa-arrow-right-to-bracket"></i> login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
